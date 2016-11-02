bdcce90225b1a02bd1fc2ed03226e37e1154d766
Alex Martynov <sinner.in.me11@gmail.com>
2016-11-02 13:31:53 +0300

    Added some not very useful shit
    Controller for user's page;
    Improved shared layout;
    Some fixes.

---
+++ FriendsGoals/Controllers/PageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FriendsGoals.Models;
+
+namespace FriendsGoals.Controllers
+{
+    public class PageController : Controller
+    {
+		public ActionResult Page(ProfileModel user) => View(user);
+
+		public ActionResult Friends() => View();
+
+		public ActionResult Messages() => View();
+    }
+}
