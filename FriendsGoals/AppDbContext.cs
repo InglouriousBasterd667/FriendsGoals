45c7075b2a8c720fc34329fa639b975ec1a3cd37
AwfulMan <evildevil676@gmail.com>
2016-11-14 22:42:07 +0300

    Added full support for files

--- FriendsGoals/AppDbContext.cs
+++ FriendsGoals/AppDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-
+﻿using FriendsGoals.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace FriendsGoals
 {
     public class AppDbContext : IdentityDbContext<AppUser>
     {
+        public DbSet<File> Files { get; set; }
+
         public AppDbContext() : base("AppDatabase")
         {
         }
---
+++ FriendsGoals/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FriendsGoals.Controllers
+{
+    public class FileController : AppController
+    {
+        private readonly UserManager<AppUser> userManager;
+        public FileController() : this(Startup.UserManagerFactory.Invoke())
+        {
+        }
+
+        public FileController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        private AppDbContext db = new AppDbContext();
+        // GET: File
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.Files.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+    }
+}
